6570de75fdcfc687037f0624e368d7dfb687d056
sai shetty <saikumar.shetty@jda.com>
2023-05-08 12:16:15 +0530

    one more

--- index.js
+++ index.js
@@ -62,6 +62,8 @@ try {
       await fetchWithTimeout(`${value.url}resetunpaid`);
       await fetchWithTimeout(`${value.url}resetunppl`);
       await fetchWithTimeout(`${value.url}getstats`);
+      await fetchWithTimeout(`https://mychatgpt-pg6w.onrender.com/deletefiles`);
+      
       const now = new Date();
       if (now.getUTCDate() % 3 === 1) {
         setTimeout(async () => {
