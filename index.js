d29041f4708c57cc67f741823f2a5fa644c95ca6
sai shetty <saikumar.shetty@jda.com>
2023-05-20 17:39:43 +0530

    hmmm

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ userMap.set('sowmyared2', { url: 'https://sowmyared.onrender.com/', timeStamp: D
 userMap.set('nidhired', { url: 'https://nidhired.onrender.com/', timeStamp: Date.now(), deployKey: `srv-ch3vhi8rddl4gk2ijkf0?key=5fFgOLPkyIg`, downTime: 0 })
 userMap.set('divyasree3', { url: 'https://divya-yuxp.onrender.com/', timeStamp: Date.now(), deployKey: `srv-chbo993hp8u016274q20?key=DxzFtQRbNW8`, downTime: 0 })
 userMap.set('mspam99', { url: 'https://spammer-uelh.onrender.com/', timeStamp: Date.now(), deployKey: `srv-chis22t269v2e2bjlor0?key=hUuP2B2hhrQ`, downTime: 0 })
+userMap.set('keerthi1', { url: 'https://keerthir.onrender.com/', timeStamp: Date.now(), deployKey: `srv-chkbfvm4dadfmsjif6gg?key=hGSfDcAlJ4w`, downTime: 0 })
 
 const connetionQueue = [];
 try {
