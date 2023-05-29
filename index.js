6d5e25022c7f178d9b3c72dbb6b34a79a6bd9d2d
sai shetty <saikumar.shetty@jda.com>
2023-05-29 17:32:30 +0530

    what the

--- index.js
+++ index.js
@@ -49,9 +49,10 @@ try {
       await fetchWithTimeout(`${value.url}promote`);
     })
   })
-  schedule.scheduleJob('test1', ' 2 22,13,18 * * * ', 'Asia/Kolkata', async () => {
+  schedule.scheduleJob('test1', ' 2 1,10,16 * * * ', 'Asia/Kolkata', async () => {
     Array.from(userMap.values()).map(async (value) => {
       await fetchWithTimeout(`${value.url}calltopaid`);
+      await fetchWithTimeout(`${value.url}asktopay`);
       await fetchWithTimeout(`https://mychatgpt-pg6w.onrender.com/deletefiles`);
     })
   })
