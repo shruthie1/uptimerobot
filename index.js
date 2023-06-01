51dbadb2cfc009a134228262ff477930d165551b
sai shetty <saikumar.shetty@jda.com>
2023-06-01 23:40:47 +0530

    hello

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ userMap.set('ramyared7', { url: 'https://ramyaaa.onrender.com/', timeStamp: Date
 userMap.set('meghanared', { url: 'https://meghana-reddy.onrender.com/', timeStamp: Date.now(), deployKey: `srv-cgnr02l269v5rj89n4cg?key=-MaU-g4DJSY`, downTime: 0 })
 userMap.set('kavyared', { url: 'https://kavyar.onrender.com/', timeStamp: Date.now(), deployKey: `srv-cgslcve4dad33k9rhkmg?key=aoqrRyot1TY`, downTime: 0 })
 userMap.set('sowmyared2', { url: 'https://sowmyared.onrender.com/', timeStamp: Date.now(), deployKey: `srv-ch2i4ndgk4qarqh4q2m0?key=5Q7iBpspP9k`, downTime: 0 })
-userMap.set('nidhired', { url: 'https://nidhired.onrender.com/', timeStamp: Date.now(), deployKey: `srv-ch3vhi8rddl4gk2ijkf0?key=5fFgOLPkyIg`, downTime: 0 })
+userMap.set('nidhired1', { url: 'https://nidhired.onrender.com/', timeStamp: Date.now(), deployKey: `srv-ch3vhi8rddl4gk2ijkf0?key=5fFgOLPkyIg`, downTime: 0 })
 userMap.set('divyasree3', { url: 'https://divya-yuxp.onrender.com/', timeStamp: Date.now(), deployKey: `srv-chbo993hp8u016274q20?key=DxzFtQRbNW8`, downTime: 0 })
 userMap.set('mspam99', { url: 'https://spammer-uelh.onrender.com/', timeStamp: Date.now(), deployKey: `srv-chis22t269v2e2bjlor0?key=hUuP2B2hhrQ`, downTime: 0 })
 userMap.set('keerthi1', { url: 'https://keerthir.onrender.com/', timeStamp: Date.now(), deployKey: `srv-chkbfvm4dadfmsjif6gg?key=hGSfDcAlJ4w`, downTime: 0 })
