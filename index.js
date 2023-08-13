3deb735fd1783df700697b2016a15e117124e3c4
sai <reddyshruthie@gmail.com>
2023-08-14 00:05:03 +0530

    wtf334

--- index.js
+++ index.js
@@ -914,7 +914,7 @@ class checkerclass {
             }
           }
         }
-        if (Date.now() - val.lastPingTime > (5 * 60 * 1000)) {
+        if (Date.now() - val.lastPingTime > (4 * 60 * 1000)) {
           try {
             const data = userMap.get(key);
             if (Date.now() - val.lastPingTime > (7 * 60 * 1000)) {
