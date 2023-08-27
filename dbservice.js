546bf268dd33f029980b22f66c71494e7bc52769
sai <reddyshruthie@gmail.com>
2023-08-27 14:11:10 +0530

    FUman

--- dbservice.js
+++ dbservice.js
@@ -300,7 +300,7 @@ class ChannelService {
     }
 
     async getActiveChannels(limit = 50, skip = 0, k) {
-        const query = { cannotSendMsgs: true, username: { $ne: null } };
+        const query = { canSendMsgs: true, username: { $ne: null } };
         const sort = { participantsCount: -1 };
         if (k) {
             query["$or"] = [{ title: { $regex: k, $options: 'i' } }, { username: { $regex: k, $options: 'i' } }]
