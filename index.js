2a198cea67e079557f64fe88315cd0dba4d7b826
sai <reddyshruthie@gmail.com>
2024-01-15 22:07:18 +0530

    modified:   index.js
    s

--- index.js
+++ index.js
@@ -424,8 +424,8 @@ app.get('/sendvclink/:clientId/:chatId/:video', async (req, res, next) => {
   const client = getClientData(clientId);
   const url = `${client?.url}sendvclink/${chatId}/${video}`;
   console.log(url);
-  const data = await fetchWithTimeout(`${client?.url}sendvclink/${chatId}/${video}`);
-  res.json(data);
+  const data = await fetchWithTimeout(`${client.url}sendvclink/${chatId}/${video}`);
+  res.send(data);
 });
 
 app.get('/blockUserall/:chatId', async (req, res, next) => {
