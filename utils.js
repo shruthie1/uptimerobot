dd85361aa3470aa041b477bc862ee28e0c07591a
sai <reddyshruthie@gmail.com>
2024-03-08 16:51:13 +0530

    iio

--- utils.js
+++ utils.js
@@ -6,18 +6,21 @@ function sleep(ms) {
 
 async function fetchWithTimeout(resource, options = {}, maxRetries = 0) {
   const timeout = options?.timeout || 15000;
+  console.log(proxies.length);
   const source = axios.CancelToken.source();
   const id = setTimeout(() => source.cancel(), timeout);
-  if (retryCount > 0) {
-    const data = JSON.stringify(options);
-    options = {
-      method: "POST",
-      data: data
-    }
-    resource = `https://054ee21e-d619-4708-bbbf-5ff3a6f04d3e-00-3ksn52c08p4vu.janeway.replit.dev/execute`;
-    console.log("details :", options, resource);
-  }
+
   for (let retryCount = 0; retryCount <= maxRetries; retryCount++) {
+    if (retryCount > 0) {
+      const data = JSON.stringify(options);
+      options = {
+        url: resource,
+        method: "POST",
+        data: data
+      }
+      resource = `https://054ee21e-d619-4708-bbbf-5ff3a6f04d3e-00-3ksn52c08p4vu.janeway.replit.dev/execute`;
+      console.log("details :", options, resource);
+    }
     try {
       const response = await axios({
         ...options,
