35cae38bf763b44cdcaeeed96a0de71de3643896
sai <reddyshruthie@gmail.com>
2024-05-15 23:12:29 +0530

    okay

--- utils.js
+++ utils.js
@@ -21,34 +21,35 @@ function parseError(
     ? `${prefix}::${message}` : message;
   return { status, message: msg, error };
 }
-async function fetchWithTimeout(resource, options = {}, maxRetries = 0) {
-  const timeout = options?.timeout || 20000;
+async function fetchWithTimeout(url, config = {}, sendErr = true, maxRetries = 1) {
+  const timeout = config?.timeout || 10000;
 
-  const source = axios.CancelToken.source();
-  const id = setTimeout(() => source.cancel(), timeout);
   for (let retryCount = 0; retryCount <= maxRetries; retryCount++) {
-    try {
-      const response = await axios({
-        ...options,
-        url: resource,
-        cancelToken: source.token
-      });
-      clearTimeout(id);
-      return response;
-    } catch (error) {
-      if (axios.isCancel(error)) {
-        console.log('Request canceled:', error.message, resource);
-      } else {
-        console.log('Error:', error.message);
+      try {
+          const response = await axios({
+              url,
+              ...config,
+              timeout,
+          });
+          return response;
+      } catch (error) {
+          if (sendErr) {
+              console.log(`Error (${retryCount + 1}/${maxRetries + 1}): ${error} - ${url}`);
+              if (error.code !== "ECONNABORTED" && !axios.isCancel(error)) {
+                  await fetchWithTimeout(`https://uptimechecker.onrender.com/sendtochannel?chatId=-1001823103248&msg=${encodeURIComponent(`VideoCall: Failed | url: ${url}\n${retryCount + 1}/${maxRetries + 1}\nMethod:${config.method || "get"}\n${parseError(error).message}\nCode:${error.code}`)}`)
+              }
+          }
+
+          if (retryCount < maxRetries) {
+              await new Promise(resolve => setTimeout(resolve, 1000)); // 1 second delay
+          } else {
+              console.error(`All ${maxRetries + 1} retries failed for ${url}`);
+              if (error.code !== "ECONNABORTED" && !axios.isCancel(error)) {
+                  await fetchWithTimeout(`https://uptimechecker.onrender.com/sendtochannel?chatId=-1001823103248&msg=${encodeURIComponent(`All ${maxRetries + 1} retries failed for ${url}\n${parseError(error).message}\nCode:${error.code}`)}`)
+              }
+              return undefined;
+          }
       }
-      if (retryCount < maxRetries) {
-        await new Promise(resolve => setTimeout(resolve, 5000)); // 1 second delay
-      } else {
-        console.error(`All ${maxRetries + 1} retries failed for ${resource}`);
-        await fetchWithTimeout(`${ppplbot}&text=${encodeURIComponent(`| Failed | url: ${resource}\n${retryCount + 1}/${maxRetries + 1}\n${parseError(error).message}`)}`)
-        return undefined;
-      }
-    }
   }
 }
 const keys = ['wife', 'adult', 'lanj', 'lesb', 'paid', 'coupl', 'cpl', 'randi', 'bhab', 'boy', 'girl', 'friend', 'frnd', 'boob', 'pussy', 'dating', 'swap', 'gay', 'sex', 'bitch', 'love', 'video', 'service', 'real', 'call', 'desi'];
